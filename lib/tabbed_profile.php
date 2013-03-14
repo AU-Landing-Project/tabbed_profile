c62669a6c36172d82faf88404429353c2df9c0f7
Matt Beckett <beck24@gmail.com>
2013-03-14 10:55:00 -0600

    adds 'my_status' into group profile sidebars

--- lib/tabbed_profile.php
+++ lib/tabbed_profile.php
@@ -91,6 +91,26 @@ function tabbed_profile_draw_group_profile($profile) {
 			$sidebar .= elgg_view('groups/sidebar/search', array('entity' => $group));
 		}
 		$sidebar .= elgg_view('groups/sidebar/members', array('entity' => $group));
+		
+		$subscribed = false;
+		if (elgg_is_active_plugin('notifications')) {
+			global $NOTIFICATION_HANDLERS;
+			
+			foreach ($NOTIFICATION_HANDLERS as $method => $foo) {
+				$relationship = check_entity_relationship(elgg_get_logged_in_user_guid(),
+						'notify' . $method, $guid);
+				
+				if ($relationship) {
+					$subscribed = true;
+					break;
+				}
+			}
+		}
+		
+		$sidebar .= elgg_view('groups/sidebar/my_status', array(
+			'entity' => $group,
+			'subscribed' => $subscribed
+		));
 	} else {
 		$sidebar = '';
 	}
