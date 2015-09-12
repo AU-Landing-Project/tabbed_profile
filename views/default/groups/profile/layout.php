f90daddfca4c34d8878d1d27c62224a8e7a5432d
Matt Beckett <beck24@gmail.com>
2015-09-11 22:57:16 -0700

    fix deprecation on group_gatekeeper

--- views/default/groups/profile/layout.php
+++ views/default/groups/profile/layout.php
@@ -13,7 +13,7 @@ if (!$profile || ($profile && $profile->widget_profile_display == 'yes')) {
   echo elgg_view('groups/profile/summary', $vars);
 }
 
-if (group_gatekeeper(false)) {
+if (elgg_group_gatekeeper(false)) {
 	echo elgg_view('groups/profile/widgets', $vars);
 } else {
 	echo elgg_view('groups/profile/closed_membership', $vars);
